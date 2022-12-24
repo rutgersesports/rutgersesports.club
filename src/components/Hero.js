1b73f639455d2e7d03cba39f51b7e5d324fb0532
Andrew Hong <andrewhong@myyahoo.com>
2022-12-24 18:47:52 -0500

    Rewrote hero background component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,12 @@
 import './Hero.css'
-import heroBackground from '../images/hero-bg-placeholder.jpg'
 
 export default function Hero() {
     return <div className='hero'>
-        <img src={heroBackground} alt='Hero Background'/>
         <div className='desc'>
-            <h1>Home to casual
-                and competitive
-                gaming at Rutgers</h1>
+            <h1>Home to casual<br/>
+                and competitive<br/>
+                gaming at Rutgers
+            </h1>
         </div>
     </div>
 }
