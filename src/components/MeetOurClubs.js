581902fe2376a3c2683b45602f0144695f8402d2
Andrew Hong <andrewhong@myyahoo.com>
2022-12-25 18:13:12 -0500

    Implement draggable scollbar

--- src/components/MeetOurClubs.js
+++ src/components/MeetOurClubs.js
@@ -1,9 +1,7 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import React from 'react';
+import ScrollContainer from 'react-indiana-drag-scroll'
 import "./MeetOurClubs.css";
 import { clubs } from "./ClubsInfo";
-import discordIcon from '../icons/discord.svg';
 
 const images = require.context("../images", true);
 
@@ -16,32 +14,12 @@ function buildClubWidget(name, getInvolved, backgroundImage, socialMedia) {
   );
 }
 
-const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 3 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
-  };
-
 export default function MeetOurClubs() {
   return (
     <div className="meet-our-clubs">
       <h1>Meet Our Clubs</h1>
-      <Carousel
-        responsive={responsive}
-        swipeable={false}
-        draggable={false}
+      <ScrollContainer
+        className="clubs"
       >
         {clubs.map((data, key) => {
           return (
@@ -51,7 +29,7 @@ export default function MeetOurClubs() {
             </div>
           );
         })}
-      </Carousel>
+      </ScrollContainer>
     </div>
   );
 }
