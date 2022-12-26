68624680f0527226eeb36a8bfa2c920a8d7e8759
Andrew Hong <andrewhong@myyahoo.com>
2022-12-26 02:20:08 -0500

    Add relative info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,39 @@ import './Footer.css';
 
 export default function Footer() {
     return <div className='footer'>
-        <p>
-            <strong>Rutgers Esports Website</strong><br/>
-            Built & Designed by <a href="https://github.com/novialriptide">Andrew Hong</a><br/>
-        </p>
+        <div className='footer-grid'>
+            <div className='footer-block'>
+                <p>
+                    <h3>Rutgers Esports</h3><br/>
+                    Part of <a href="http://sca.rutgers.edu/">Rutgers University Student Center and Activities</a><br/>
+                    604 Bartholomew Rd, Piscataway, New Jersey, United States, 08854
+                </p>
+            </div>
+            <div className='footer-block'>
+                <p>
+                    <h3>Related Links</h3><br/>
+                    <a href="https://github.com/rutgersesports/rutgersverifybot">Email Verification Discord Bot</a><br/>
+                    <a href="admin-login">Adminstrator Login</a><br/>
+                    <a href="apply">Job Positions</a><br/>
+                </p>
+            </div>
+            <div className='footer-block'>
+                <p>
+                    <h3>Rutgers Esports Social Media</h3><br/>
+                    <a href="https://discord.gg/rutgersesports">Discord</a><br/>
+                    <a href="https://twitch.tv/rutgersesports">Twitch</a><br/>
+                    <a href="https://twitter.com/RutgersEsports">Twitter</a><br/>
+                    <a href="https://instagram.com/rutgersesports">Instagram</a><br/>
+                    <a href="https://facebook.com/rutgersesports">Facebook</a><br/>
+                    <a href="https://youtube.com/rutgersesports">Youtube</a><br/>
+                    <a href="https://www.linkedin.com/company/rutgers-esports">LinkedIn</a><br/>
+                    <a href="https://github.com/rutgersesports">GitHub</a><br/>
+                    <a href="mailto: rutgersesports@gmail.com">Email</a><br/>
+                </p>
+            </div>
+        </div>
+        <div className='footer-end'>
+            Website Built & Designed by <a href="https://github.com/novialriptide">Andrew Hong</a><br/>
+        </div>
     </div>
 }
