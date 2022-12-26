10c26b0d8279d55996f68b54c66019228e87cc41
Andrew Hong <andrewhong@myyahoo.com>
2022-12-26 15:30:47 -0500

    Create JobsInfo.js

---
+++ src/data/JobsInfo.js
@@ -0,0 +1,240 @@
+export const jobs = [
+  {
+    teamName: "Graphic Design",
+    roles: [
+      {
+        name: "Graphic Designer",
+        specialResponsibilities: [],
+        specialPreferredQualities: [],
+      },
+    ],
+    desc:
+      "Our Graphic Design team is dedicated to creating visual content for the Rutgers Esports community. Graphic Designers use the Adobe Creative Cloud applications to create graphic logos, banners, title screens, overlays, buttons, stream assets, partnership announcements, etc. Anything that is visual coming from Rutgers Esports onto social media, events, clubs, etc. usually comes from this team.",
+    responsibilities: [
+      "Ability to be organized, work within project deadlines, attend meetings in a timely manner",
+      "Create graphic content as needed by other teams",
+      "Design merchandise for Rutgers Esports such as buttons, stickers, phone cases, apparel, etc",
+      "Give and receive constructive criticism within your team",
+      "Understanding at least one factor in graphic design (color theory, layout, spacing)",
+      "Contribute fresh and new ideas for our flagship event flyers and potential side projects",
+    ],
+    preferredQualities: [
+      "Experience in Adobe products such as PhotoShop, Illustrator, and/or Indesign",
+      "Previous experience in graphic design, art, or photo editing",
+      "Knowledge of modern graphic design trends",
+    ],
+    form: "https://forms.gle/HnptTQcEN5RP2WhQ7",
+  },
+  {
+    teamName: "Media Production",
+    roles: [
+      {
+        name: "Video Editor",
+        specialResponsibilities: [],
+        specialPreferredQualities: [],
+      },
+      {
+        name: "Cinematographer",
+        specialResponsibilities: [],
+        specialPreferredQualities: [],
+      },
+      {
+        name: "Writer",
+        specialResponsibilities: [],
+        specialPreferredQualities: [],
+      },
+    ],
+    desc:
+      "Our Media Production team is looking for experienced videographers/aspiring videographers comfortable both with shooting video and editing with editing software (Adobe Premiere Pro, Sony Vegas, After Effects, etc.). Future prospects include creating content to post on YouTube and focusing on our YouTube channel. Even if you're not well-versed with editing software, video looks for great storytellers and ideas from all members!Our Media Production team is looking for experienced videographers/aspiring videographers comfortable both with shooting video and editing with editing software (Adobe Premiere Pro, Sony Vegas, After Effects, etc.). Future prospects include creating content to post on YouTube and focusing on our YouTube channel. Even if you're not well-versed with editing software, video looks for great storytellers and ideas from all members! Note: The Video team is looking for anyone who's willing to contribute small ideas, help coordinate filmings, and other types of small jobs. Don\t be too worried if you have a strong interest but lack experience with any of the above specializations!",
+    responsibilities: [
+      "Work with a team to complete tasks within project deadlines",
+      "Follow the directions of the director",
+      "Plan and draft a variety of videos (skits, interviews, recaps, etc)",
+      "Upload videos or clips to our YouTube channel to help it grow",
+      "Capture and edit videos from events, club meetings, and meetups at varying locations",
+      "Interviews of competitive and casual players on their experiences at events",
+      "Pitch minor/major video ideas to post on social media to promote events",
+    ],
+    preferredQualities: [
+      "Work well with others",
+      "Experience with video equipment (DLSR camera, etc)",
+      "Experience with video editing software (Adobe After Effects & Premiere Pro)",
+      "Able to produce ideas, innovation, and creativity",
+      "Capable of managing time wisely",
+    ],
+    form: "https://forms.gle/8raPQ4AtPxrLfmju8",
+  },
+  {
+    teamName: "Technology",
+    roles: [
+      {
+        name: "Web Developer",
+        specialResponsibilities: [
+          "Experience with JavaScript, HTML, & CSS",
+          "Experience with ReactJS",
+        ],
+        specialPreferredQualities: [],
+      },
+      {
+        name: "Discord Bot Developer",
+        specialResponsibilities: [
+          "Experience with Python",
+          "Experience with any type of Discord Bot framework (discordpy, pycord, discordjs, discordphp, discord.net, hikari)",
+        ],
+        specialPreferredQualities: [
+          "Experience with SQL, MongoDB, or Firebase",
+        ],
+      },
+      {
+        name: "Database Engineer",
+        specialResponsibilities: [
+          "Experience with Python",
+          "Experience with SQL, MongoDB, or Firebase",
+        ],
+        specialPreferredQualities: [
+          "Experience with a Java-like syntax language (C#, PHP)",
+        ],
+      },
+    ],
+    desc:
+      "As one of our technology developers, you will be able to contribute to the development and maintenance of the Rutgers Esports technologies! This includes our website, discord bot, and inventory database system. Tech Officers vary from front end to back end development.",
+    responsibilities: ["Experience with Git", "Must own a GitHub account"],
+    preferredQualities: ["Willingness to learn"],
+    form: "https://forms.gle/wVhWd5CWr8zaxANy9",
+  },
+  {
+    teamName: "Competitive",
+    roles: [
+      {
+        name: "Team Member",
+        specialResponsibilities: [],
+        specialPreferredQualities: [],
+      },
+    ],
+    desc:
+      "This team is responsible for managing the competitive club teams within Rutgers Esports. During events, you will be responsible for officiating our various tournaments. To join this team, you will need a love of competitive esports, have a need to self-improve, and be able to give and take constructive criticism. Rutgers Esports is consistently a Top 16 school, always striving to be #1.",
+    responsibilities: [
+      "Formalize a communication stream between GMs and Content/SM to report tournaments, placements, tryouts, etc",
+      "Communicate with competitive teams to ensure their needs are being heard",
+      "Keep track of which leagues our esports teams are part of",
+      "Work with team staff to ensure players and teams are adhering to contract requirements (if applicable)",
+      "Answer questions and solve problems with tournament registration both before and during events",
+      "Officiate tournaments at events",
+      "Communicate needs for events",
+      "Training new GMs when necessary",
+    ],
+    preferredQualities: [
+      "Indepth knowledge of competition and esports history",
+      "Practical understanding of different styles of tournaments (seeding, double-elimination, round robin, swiss rounds, etc)",
+      "Holistic approach to fostering player growth",
+      "Friendly, but firm personality",
+    ],
+    form: "https://forms.gle/mBUkQhryZhDjc7YH8",
+  },
+  {
+    teamName: "Human Resources",
+    roles: [
+      {
+        name: "Team Member",
+        specialResponsibilities: [],
+        specialPreferredQualities: [],
+      },
+    ],
+    desc:
+      "The Human Resources team handles everything involving the internal affairs of Rutgers Esports. If you’re looking for viable experience in recruitment (emailing, scheduling, interviewing, onboarding), managing officer relationships, and helping organize the schedule for major events, this team is for you!",
+    responsibilities: [
+      "Design and carry out recruitment/onboarding for new officers for the fall, spring, and summer semesters",
+      "Monitoring officer attendance records",
+      "Observe productivity and wellbeing of members",
+      "Serve as a direct resource for other officers such as solving conflicts, giving advice, or just being someone who listens",
+      "Managing event staff during major events",
+      "Organize volunteers for events and officer programs",
+    ],
+    preferredQualities: [
+      "Excellent communicator",
+      "Active listener",
+      "Organized",
+      "Experience with Excel/Google Sheets and Discord",
+    ],
+    form: "https://forms.gle/LRpPzXgMrT8aCZcR6",
+  },
+  {
+    teamName: "Social Media",
+    roles: [
+      {
+        name: "Team Member",
+        specialResponsibilities: [],
+        specialPreferredQualities: [],
+      },
+    ],
+    desc:
+      "Our Social Media team is looking for a candidate who can help us expand our brand online. The ideal candidate can utilize the most popular social media channels (Facebook, Twitter, Instagram, Snapchat, Discord, etc.) and can create original content that engages our audience and helps maintain/grow our organizational brand.",
+    responsibilities: [
+      "Create original and quality posts and captions for our social media platforms",
+      "Provide the Rutgers Esports community with updated information such as events, tryouts, recruitment, contests, clubs, and any esports news",
+      "Actively engage with the online community by interacting with both the Rutgers Esports audience as well as professioanls and teams within the realm of esports",
+      "Contribute fresh and new ideas to the overrall strategy by discussing and presenting new projects, campaigns, or initiatives to the team and director",
+      "Maintain a consistent posting schedule by utilizing our calendar, making consistent updates and changes when needed",
+    ],
+    preferredQualities: [
+      "Experience using major social media channels for more than just personal usage",
+      "Experience working in teams and leadership roles",
+      "Can commit to monitoring and recognizing major esports events as well as having knowledge on any of our supported games' scenes",
+      "Keeps up to date using Google Calendar",
+    ],
+    form: "https://forms.gle/8TKkaTs7bNgZ8p5A8",
+  },
+  {
+    teamName: "Broadcasting",
+    roles: [
+      {
+        name: "Broadcaster",
+        specialResponsibilities: ["Capable of running a live-stream"],
+        specialPreferredQualities: [],
+      },
+      {
+        name: "Stream Producer",
+        specialResponsibilities: [
+          "Able to organize and prepare for a live-stream",
+        ],
+        specialPreferredQualities: [],
+      },
+    ],
+    desc:
+      "Our broadcasting team is looking for experienced streamers or those knowledgeable in production to assist with broadcasting our events, tournaments, and community-related content onto Twitch. ",
+    responsibilities: [
+      "Setup the stream computer and all necessary broadcasting equipment at the LAN center (lights, camera, observer station, etc)",
+      "Run Vmix through the management and creation of scenes",
+      "Able to read and interact with the community in live-stream chats",
+      "Logistical planning and organization skills of virtual events",
+      "Connecting with casters/observers from their respective game community",
+    ],
+    preferredQualities: [
+      "Experience with Vmix, StreamLabs, or any other relevant streaming software",
+      "Familiar with Twitch and Twitch bots",
+      "Proper technology to stream and nitro boost",
+      "Experience with working in teams",
+      "Quick thinking and adaptability",
+      "Experience in event planning",
+    ],
+    form: "https://forms.gle/WVyjs4CQoKWYtBGY9",
+  },
+  {
+    teamName: "Inactive Role",
+    roles: [
+      {
+        name: "Broadcaster",
+        specialResponsibilities: ["Capable of running a live-stream"],
+        specialPreferredQualities: [],
+      },
+      {
+        name: "Stream Producer",
+        specialResponsibilities: [
+          "Able to organize and prepare for a live-stream",
+        ],
+        specialPreferredQualities: [],
+      },
+    ],
+    form: "https://forms.gle/gKMwAqwSxq7u9Zbj8",
+  },
+];
