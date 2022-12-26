67130beb38a1bfcfd2ddbb56d47ae214b6b409d4
Andrew Hong <andrewhong@myyahoo.com>
2022-12-25 21:29:33 -0500

    Description rewrite

--- src/components/AboutOurselves.js
+++ src/components/AboutOurselves.js
@@ -8,12 +8,14 @@ export default function AboutOurselves() {
         <div className='about-ourselves-desc'>
             <img src={communityImage} alt='Community'/>
             <p>
-                <strong>Community</strong><br/>
-                We pride ourselves on being an open, friendly, and diverse community. Everyone is welcome here no matter who you are or where you come from.<br/>
-                <strong>Play Casually</strong><br/>
-                Whether you play to win or play for fun, we have a place for you with nearly a dozen clubs dedicated to the most popular video games.<br/>
-                <strong>Play Competitively</strong><br/>
-                Our teams and players are some of the best in the nation with top placings in many of the most well-known collegiate esports tournaments.<br/>
+                Rutgers Esports is a large community focused on providing a welcoming
+                and inclusive environment for the gaming community at Rutgers University.
+                We pride ourselves on being an open, friendly, and diverse community.
+                Everyone is welcome here no matter who you are or where you come from.
+                Whether you play to win or play for fun, we have a place for you with
+                nearly a dozen clubs dedicated to the most popular video games. Our teams
+                and players are some of the best in the nation with top placings in many
+                of the most well-known collegiate esports tournaments.
             </p>
         </div>
     </div>
