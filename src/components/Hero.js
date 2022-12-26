21d7af8558bac1f5f1867c35492b63b24ab2e533
Andrew Hong <andrewhong@myyahoo.com>
2022-12-25 19:31:49 -0500

    Implement video background

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import './Hero.css'
+import backgroundVideo from '../videos/hero-background.mp4';
+import './Hero.css';
 
 export default function Hero() {
     return <div className='hero'>
+        <video id="background-video" playsInline="playsInline" autoPlay="autoPlay" muted="muted" loop="loop">
+            <source src={backgroundVideo} type="video/mp4"/>
+        </video>
         <div className='hero-text'>
             <h1>Home to casual<br/>
                 and competitive<br/>
