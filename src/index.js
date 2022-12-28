0ed00fbbfa7fccceea43d40987b550333609f416
Andrew Hong <andrewhong@myyahoo.com>
2022-12-27 20:11:55 -0500

    Some cleanup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './pages/HomePage';
+import './index.css';
+import Apply from './pages/Apply';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/apply" element={<Apply/>}/>
+      </Routes>
     </HashRouter>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
