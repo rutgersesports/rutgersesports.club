0610534e26838a7d181662ae3c684f70310d5b2e
Andrew Hong <andrewhong@myyahoo.com>
2023-01-06 00:27:11 -0500

    Rework footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,131 +1,60 @@
 import React from "react";
+import discordIcon from "../icons/discord.svg"
+import facebookIcon from "../icons/facebook.svg"
+import instagramIcon from "../icons/instagram.svg"
+import twitchIcon from "../icons/twitch.svg"
+import twitterIcon from "../icons/twitter.svg"
+import linkedinIcon from "../icons/linkedin.svg"
+import githubIcon from "../icons/github.svg"
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
   return (
+    <>
     <div className="footer">
-      <div className="footer-grid">
-        <div className="footer-block">
-          <p>
-            <h3>Rutgers Esports</h3>
-            <br />
-            Part of{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="http://sca.rutgers.edu/"
-            >
-              Rutgers University Student Center and Activities
-            </a>
-            <br />
-            604 Bartholomew Rd, Piscataway, New Jersey, United States, 08854
-          </p>
+      <div className="footer-row">
+        <div className="footer-col">
+          <h3>Rutgers Esports</h3>
+          Rutgers Esports is a student-run collegiate esports organization at
+          Rutgers University - New Brunswick. We have no affiliation with the
+          Esports Center located on Busch Campus; for all Esports Center
+          inquiries, please visit esports.rutgers.edu.
         </div>
-        <div className="footer-block">
-          <p>
-            <h3>Related Links</h3>
-            <br />
-            <a href="https://github.com/rutgersesports/rutgersverifybot">
+        <div className="footer-col">
+          <h3>Relative Links</h3>
+          <ul>
+            <li><a href="https://github.com/rutgersesports/rutgersverifybot">
               Email Verification Discord Bot
-            </a>
-            <br />
-            <a href="admin-login">Adminstrator Login</a>
-            <br />
-            <Link to="apply">Job Positions</Link>
-            <br />
-          </p>
+            </a></li>
+            <li><a href="https://github.com/rutgersesports/rutgersesports.club">
+              Website Source Code
+            </a></li>
+            <li><Link to="apply">Volunteer Positions</Link></li>
+          </ul>
+          <div className="socials">
+            <img src={discordIcon} className="social-icon" alt="social-icon"/>
+            <img src={facebookIcon} className="social-icon" alt="social-icon"/>
+            <img src={instagramIcon} className="social-icon" alt="social-icon"/>
+            <img src={twitchIcon} className="social-icon" alt="social-icon"/>
+            <img src={twitterIcon} className="social-icon" alt="social-icon"/>
+            <img src={linkedinIcon} className="social-icon" alt="social-icon"/>
+            <img src={githubIcon} className="social-icon" alt="social-icon"/>
+          </div>
         </div>
-        <div className="footer-block">
-          <p>
-            <h3>Social Media</h3>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://discord.gg/rutgersesports"
-            >
-              Discord
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitch.tv/rutgersesports"
-            >
-              Twitch
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitter.com/RutgersEsports"
-            >
-              Twitter
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://instagram.com/rutgersesports"
-            >
-              Instagram
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://facebook.com/rutgersesports"
-            >
-              Facebook
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://youtube.com/rutgersesports"
-            >
-              Youtube
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/company/rutgers-esports"
-            >
-              LinkedIn
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/rutgersesports"
-            >
-              GitHub
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="mailto: rutgersesports@gmail.com"
-            >
-              Email
-            </a>
-            <br />
-          </p>
-        </div>
-      </div>
-      <div className="footer-end">
-        Website Built & Designed by{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/novialriptide"
-        >
-          Andrew Hong
-        </a>
-        <br />
       </div>
     </div>
+    <div className="footer-end">
+      Original Website Built & Designed by{" "}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/novialriptide"
+      >
+        Andrew Hong
+      </a>
+      <br />
+    </div>
+    </>
   );
 }
