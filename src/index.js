9f83661688ec3bf7dd7897004bd364c8f6a7dea1
Andrew Hong <andrewhong@myyahoo.com>
2023-01-23 04:31:13 -0500

    Hail mary

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import HomePage from "./pages/HomePage";
 import "./index.css";
 import Apply from "./pages/Apply";
 
 
+function App() {
+  const pathname = useLocation().pathname
+
+  /*
+   * Yes, this is the most shittest way of handling multiple
+   * pages in React. Does it work? Yes it does.
+   */
+  if (pathname === "/") {
+    return <HomePage/>
+  }
+  else if (pathname === "/apply") {
+    return <Apply/>
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/apply" component={Apply} />
+        <Route component={App} />
       </Switch>
     </BrowserRouter>
   </React.StrictMode>
