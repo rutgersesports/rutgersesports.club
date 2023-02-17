8d41bdcf825ab1b31ff866984458415cfbaaf1fa
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-02-17 05:30:54 -0500

    Remove discord icon in favor of a cleaner design

--- src/components/meet-our-clubs.jsx
+++ src/components/meet-our-clubs.jsx
@@ -4,37 +4,30 @@ import styles from '../styles/meet-our-clubs.module.css'
 import { clubs } from '../data/clubs-info'
 import Image from 'next/image'
 
-const images = require.context('../images', true)
-const icons = require.context('../icons', true)
-
 function buildClubWidget(name, getInvolved, backgroundImage, socialMedia, x, y) {
-  const loadedImage = images(backgroundImage)
   return (
-    <div
-      className={styles["club"]}
-      style={{
-        backgroundImage: `url(${loadedImage})`,
-        start: { x },
-        top: { y },
-      }}
-    >
-      <div className={styles["club-contents"]}>
-        <h2>{name}</h2>
-        <div id={styles["club-links"]}>
-          <a target="_blank" rel="noopener noreferrer" href={socialMedia.discord}>
-            <Image src={icons('./discord.svg')} alt="discord" id="social-media-icon" />
-          </a>
+    <a style={{textDecoration: 'none'}} target="_blank" rel="noopener noreferrer" href={socialMedia.discord}>
+      <div
+        className={styles['club']}
+        style={{
+          backgroundImage: `url(${backgroundImage.src})`,
+          start: { x },
+          top: { y },
+        }}
+      >
+        <div className={styles['club-contents']}>
+          <h2>{name}</h2>
         </div>
       </div>
-    </div>
+    </a>
   )
 }
 
 export default function MeetOurClubs() {
   return (
-    <div className={styles["meet-our-clubs"]}>
+    <div className={styles['meet-our-clubs']}>
       <h1>Meet Our Clubs</h1>
-      <ScrollContainer className={styles["clubs"]} hideScrollbars={false}>
+      <ScrollContainer className={styles['clubs']} hideScrollbars={false}>
         {clubs.map((data, key) => {
           return (
             <div key={key}>
