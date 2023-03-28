764d6d10f2919e1f4c3414ca4bbe2e779a3644d6
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-03-28 06:01:19 -0400

    Responsive stuff

--- src/components/navigation-bar.jsx
+++ src/components/navigation-bar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import menuIcon from '../icons/menu.svg'
 import rutgersEsportsLogo from '../images/rutgersesports-logo.png'
 import styles from '../styles/navigation-bar.module.css'
 import Image from 'next/image'
 
-export default function NavigationBar() {
+function NavigationBar() {
+  const [isOpen, setOpen] = useState(true)
+
+  function handleMenuClick() {
+    setOpen(!isOpen)
+  }
+
   return (
     <div className={styles['nav']}>
       <div className={styles['esports-logo']}>
@@ -12,18 +19,27 @@ export default function NavigationBar() {
           <Image src={rutgersEsportsLogo} alt="Rutgers Esports Logo" />
         </Link>
       </div>
-      <ul>
-        <li>
-          <div className={styles['route']}>
-            <Link href="/">Home</Link>
-          </div>
-        </li>
-        <li>
-          <div className={styles['route']}>
-            <Link href="/apply">Apply</Link>
-          </div>
-        </li>
-      </ul>
+      <div className={styles['menu']}>
+        <div className={styles['mobile-menu-icon']} onClick={handleMenuClick}>
+          <Image src={menuIcon} alt="menu" />
+        </div>
+        <div style={{ display: isOpen ? 'block' : 'none' }}>
+          <ul>
+            <li>
+              <div className={styles['route']}>
+                <Link href="/">Home</Link>
+              </div>
+            </li>
+            <li>
+              <div className={styles['route']}>
+                <Link href="/apply">Apply</Link>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default NavigationBar
