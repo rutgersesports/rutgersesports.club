37a1c49e8846efcf023a4b0a9a8ff1d62b09fc36
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-08-23 20:11:12 -0400

    chatgpt-ed "contact us" card descriptions

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -10,26 +10,37 @@ export default function ContactUs() {
         <div className={styles['desc-col']}>
           <div className={styles['desc-col-content']}>
             <h2>Partners</h2>
-            <p>Interested in sponsoring or partnering with Rutgers Esports?</p>
+            <p>
+              If you or your organization would like to do a collaboration through sponsorship or partnership, we
+              encourage you to reach out to us.
+            </p>
           </div>
           <Link href="mailto: partnerships.rutgersesports@gmail.com">
             <div className={styles['info-button']}>Contact Partnerships</div>
           </Link>
         </div>
+
         <div className={styles['desc-col']}>
           <div className={styles['desc-col-content']}>
             <h2>Events</h2>
-            <p>Questions about events or artists alley at Rutgers Esports.</p>
+            <p>
+              Send us an email if you have any questions relating to our events such as Scarlet Classic, Artist Alley,
+              Scarlet Knightmare, etc.
+            </p>
           </div>
 
           <Link href="mailto: rutgersesports@gmail.com">
             <div className={styles['info-button']}>Contact Events</div>
           </Link>
         </div>
+
         <div className={styles['desc-col']}>
           <div className={styles['desc-col-content']}>
             <h2>Applications</h2>
-            <p>For questions regarding applications to Rutgers Esports.</p>
+            <p>
+              If you happen to have any inquiries related to your application, we are here and more than willing to
+              provide you with the necessary guidance.
+            </p>
           </div>
 
           <Link href="mailto: ruesportshr@gmail.com">
