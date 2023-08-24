55a8d71efad0603a74561dd5633023ddb5338519
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-08-24 18:35:10 -0400

    Improve "Contact Us" code

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -2,60 +2,53 @@ import React from 'react'
 import styles from '../styles/contact-us.module.css'
 import Link from 'next/link'
 
+interface IContactCard {
+  title: string
+  description: string
+  link: string
+  linkTitle: string
+}
+
+function ContactCard(props: IContactCard) {
+  return (
+    <div className={styles['desc-col']}>
+      <div className={styles['desc-col-content']}>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </div>
+      <Link href={props.link}>
+        <div className={styles['info-button']}>{props.linkTitle}</div>
+      </Link>
+    </div>
+  )
+}
+
 export default function ContactUs() {
   return (
     <div className={styles['contact-us']}>
       <h1>Contact Us</h1>
       <div className={styles['contact-us-desc']}>
-        <div className={styles['desc-col']}>
-          <div className={styles['desc-col-content']}>
-            <h2>Partners</h2>
-            <p>
-              If you or your organization would like to do a collaboration through sponsorship or partnership, we
-              encourage you to reach out to us.
-            </p>
-          </div>
-          <Link href="mailto: partnerships.rutgersesports@gmail.com">
-            <div className={styles['info-button']}>Contact Partnerships</div>
-          </Link>
-        </div>
+        <ContactCard
+          title="Partners"
+          description="If you or your organization would like to do a collaboration through sponsorship or partnership, we encourage you to reach out to us."
+          link="mailto: partnerships.rutgersesports@gmail.com"
+          linkTitle="Contact Partnerships"
+        />
 
-        <div className={styles['desc-col']}>
-          <div className={styles['desc-col-content']}>
-            <h2>Events</h2>
-            <p>
-              Send us an email if you have any questions relating to our events such as Scarlet Classic, Artist Alley,
-              Scarlet Knightmare, etc.
-            </p>
-          </div>
+        <ContactCard
+          title="Events"
+          description="Send us an email if you have any questions relating to our events such as Scarlet Classic, Artist Alley, Scarlet Knightmare, etc."
+          link="mailto: rutgersesports@gmail.com"
+          linkTitle="Contact Events"
+        />
 
-          <Link href="mailto: rutgersesports@gmail.com">
-            <div className={styles['info-button']}>Contact Events</div>
-          </Link>
-        </div>
-
-        <div className={styles['desc-col']}>
-          <div className={styles['desc-col-content']}>
-            <h2>Applications</h2>
-            <p>
-              If you happen to have any inquiries related to your application, we are here and more than willing to
-              provide you with the necessary guidance.
-            </p>
-          </div>
-
-          <Link href="mailto: ruesportshr@gmail.com">
-            <div className={styles['info-button']}>Contact Human Resources</div>
-          </Link>
-        </div>
+        <ContactCard
+          title="Applications"
+          description="If you happen to have any inquiries related to your application, we are here and more than willing to provide you with the necessary guidance."
+          link="mailto: ruesportshr@gmail.com"
+          linkTitle="Contact Human Resources"
+        />
       </div>
     </div>
   )
 }
-
-/*
-For Partnership/Sponsorship related items: Partnerships.rutgersesports@gmail.com
-For Event/Artist Alley Related items: 
-Rutgersesports@gmail.com
-For questions about applying to Rutgers Esports:
-RUesportsHR@gmail.com
-*/
