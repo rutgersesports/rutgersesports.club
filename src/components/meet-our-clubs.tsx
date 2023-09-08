a209f2bc9da6d837f4b80da71b8252ede9d9a2ca
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-09-08 19:35:07 -0400

    Randomize club order

--- src/components/meet-our-clubs.tsx
+++ src/components/meet-our-clubs.tsx
@@ -28,12 +28,40 @@ function Club(props: IClub) {
   )
 }
 
+function shuffle(array: Array<any>) {
+  /* Taken straight from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
+  let currentIndex = array.length,
+    randomIndex
+
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+  }
+
+  return array
+}
+
 export default function MeetOurClubs() {
+  const shuffledClubs: Array<any> = shuffle(clubs)
+
+  /*
+   * This is dealing with NextJS bullshit.
+   * Read here: https://stackoverflow.com/questions/72673362/error-text-content-does-not-match-server-rendered-html
+   */
+  const [hydrated, setHydrated] = React.useState<boolean>(false)
+  React.useEffect(() => {
+    setHydrated(true)
+  }, [])
+  if (!hydrated) {
+    return null
+  }
+
   return (
     <div className={styles['meet-our-clubs']}>
       <h1>Meet Our Clubs</h1>
       <ScrollContainer className={styles['clubs']} hideScrollbars={false}>
-        {clubs.map((data, key) => {
+        {shuffledClubs.map((data, key) => {
           return <Club key={key} name={data.name} backgroundImage={data.image} socialMedia={data.socialMedia} />
         })}
       </ScrollContainer>
