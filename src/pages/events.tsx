f56a36e6e7a80aa3ac1b199d4688e6c417cd0031
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-09-24 09:55:01 -0400

    Create events.tsx

---
+++ src/pages/events.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/base.module.css'
+import NavigationBar from '../components/navigation-bar'
+import Footer from '../components/footer'
+import ImageHeader from '@/components/image-header'
+
+export default function Events() {
+  return (
+    <div className={styles['base']}>
+      <NavigationBar />
+      <ImageHeader title="Events" />
+      <Footer />
+    </div>
+  )
+}
