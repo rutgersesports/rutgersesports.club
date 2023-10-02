1e8f82a4874f00fbb03ce818c8fab0176062a35c
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-10-02 15:11:00 -0400

    Generalize ImageHeader

--- src/components/image-header.tsx
+++ src/components/image-header.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import styles from '../styles/job-openings-header.module.css'
+import { StaticImageData } from 'next/image'
 
 interface IImageHeader {
   title: string
+  backgroundImage: StaticImageData
 }
 
 export default function ImageHeader(props: IImageHeader) {
   return (
-    <div className={styles['job-openings-header']}>
+    <div
+      className={styles['job-openings-header']}
+      style={{ backgroundImage: `url(${props.backgroundImage.src})`, backgroundRepeat: 'no-repeat' }}
+    >
       <h1>{props.title}</h1>
     </div>
   )
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -3,12 +3,13 @@ import styles from '../styles/base.module.css'
 import NavigationBar from '../components/navigation-bar'
 import Footer from '../components/footer'
 import ImageHeader from '@/components/image-header'
+import eventsImage from '../images/anthpo.jpg'
 
 export default function Events() {
   return (
     <div className={styles['base']}>
       <NavigationBar />
-      <ImageHeader title="Events" />
+      <ImageHeader title="Events" backgroundImage={eventsImage} />
       <Footer />
     </div>
   )
