cdabf21c826ef01c1291cef9faf7cc52138e78e8
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-10-22 17:53:05 -0400

    Add schedule visuals

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -1,3 +1,5 @@
+import styles from '../styles/schedule.module.css'
+
 export type ScheduleEvent = {
   name: string
   location: string
@@ -11,18 +13,47 @@ interface IScheduleRow {
 }
 
 function ScheduleRow(props: IScheduleRow) {
-  const startMinutes = props.event.startTime?.getMinutes()
-  const endMinutes = props.event.endTime?.getMinutes()
-  const displayTime = `${props.event.startTime?.getHours()}:${startMinutes} - ${props.event.endTime?.getHours()}:${endMinutes}`
+  let startHour = props.event.startTime?.getHours()
+  const startMinutes = props.event.startTime?.getMinutes().toString().padStart(2, '0')
+  let endHour = props.event.endTime?.getHours()
+  const endMinutes = props.event.endTime?.getMinutes().toString().padStart(2, '0')
+
+  let startSuffix = 'AM'
+  let endSuffix = 'AM'
+
+  if (startHour != undefined && endHour != undefined) {
+    if (startHour >= 12) {
+      startSuffix = 'PM'
+    }
+    if (startHour > 12) {
+      startHour -= 12
+    }
+    if (endHour >= 12) {
+      endSuffix = 'PM'
+    }
+    if (endHour > 12) {
+      endHour -= 12
+    }
+  }
+
+  const displayTime = `${startHour}:${startMinutes}${startSuffix} - ${endHour}:${endMinutes}${endSuffix}`
 
   return (
     <tr>
       <th>
-        <p>{displayTime}</p>
+        <p style={{ fontWeight: 'bold' }}>
+          {startMinutes != undefined ? (
+            <p style={{ margin: 0 }}>{displayTime}</p>
+          ) : (
+            <p style={{ margin: 0 }}>All Day</p>
+          )}
+        </p>
       </th>
       <th>
-        <strong>{props.event.name}</strong>
-        <p>{props.event.location}</p>
+        <div style={{ marginTop: '10px', marginBottom: '10px' }}>
+          <p style={{ margin: 0, fontWeight: 'bold' }}>{props.event.name}</p>
+          <p style={{ margin: 0 }}>{props.event.location}</p>
+        </div>
       </th>
     </tr>
   )
@@ -37,9 +68,13 @@ export default function Schedule(props: ISchedule) {
   return (
     <div>
       <h1>{props.title}</h1>
-      {props.events.map((data, key) => {
-        return <ScheduleRow key={key} event={data} />
-      })}
+      <table className={styles['schedule']}>
+        <tbody>
+          {props.events.map((data, key) => {
+            return <ScheduleRow key={key} event={data} />
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
