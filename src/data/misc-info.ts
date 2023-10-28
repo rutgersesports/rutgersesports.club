a2383cd36cbe5d9addc1db99e5316a0d1ad394a3
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-10-28 11:48:15 -0400

    Change message

--- src/data/misc-info.ts
+++ src/data/misc-info.ts
@@ -2,5 +2,4 @@
  * Set the variable constant to `null` if you want to no longer show a message.
  */
 
-export const alertMessage: string =
-  'The Esports Center on Busch Campus is closed until further notice. We apologize for the inconvenience.'
+export const alertMessage: string = 'Scarlet Knightmare begins at 12PM'
