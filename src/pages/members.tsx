bcabe02456341e2cac9e9a44e0b315023e56c1cf
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-09-20 15:43:49 -0400

    Small changes

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -103,7 +103,7 @@ function MemberRow(props: IMemberRow) {
       <p style={{ fontSize: '2.15rem', fontWeight: 'bolder', textAlign: 'left', marginBottom: '0', color: '#a51f1f' }}>
         {props.startYear} - {props.startYear + 1}
       </p>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'left' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {members.map((displayMember: DisplayMember, i: number) => {
           return <MemberCard displayMember={displayMember} key={i} />
         })}
@@ -125,13 +125,6 @@ export default function HomePage() {
             <div>
               <p style={{ fontWeight: 'bolder', fontSize: '2.6rem', marginBottom: '0', color: '#a51f1f' }}>
                 Rutgers Esports Executive Board & Alumni
-              </p>
-              <p>
-                Positions older than 2022 may not be accurate as there are no records of previous members stored other
-                than the wayback machine from the legacy website and reaching out to alumni to recall their memories.
-                Members might also not have correct titles due to promotions or impeachments during the school year;
-                only their final position title is shown. If you are in this list and wish to be removed or edited, send
-                us an email: ruesportshr@gmail.com
               </p>
               {Array(endYear - startYear)
                 .fill('_')
